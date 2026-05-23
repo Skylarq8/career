e378a0d9e2f68f7d809836289a5fe7df420c2148
Nrn Badar-Uugan <macbook@192.168.1.7>
2026-05-23 12:43:21 +0800

    Point frontend to deployed Convex preview

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,7 +3,7 @@
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { useState } from "react";
 
-const fallbackConvexUrl = "https://robust-puffin-848.eu-west-1.convex.cloud";
+const fallbackConvexUrl = "https://brave-chickadee-230.eu-west-1.convex.cloud";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL || fallbackConvexUrl;
